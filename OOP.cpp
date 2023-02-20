8303f58013c9538180c921795df9fffdbb29d0cb
Art Singletary <artsingletary@yahoo.com>
2023-02-20 12:58:48 -0500

    Created OOP.cpp

---
+++ OOP.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdlib.h>
+using std::string;
+
+
+class Employee {
+public:
+     string Name; 
+     string Company;
+     int Age;
+     void IntroduceYourself() {
+         std::cout<<"Name: "<< Name << std::endl;
+         std::cout << "Company: " << Company << std::endl;
+         std::cout << "Age: " << Age << std::endl;
+     }
+
+};
+
+int main()
+{
+    Employee employee1; 
+    employee1.Name = "Art";
+    employee1.Company = "TSA";
+    employee1.Age = 54;
+    employee1.IntroduceYourself();
+}
+
+
+
